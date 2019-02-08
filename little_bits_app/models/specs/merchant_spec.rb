00e4b847e220a431926c8464d3811b7c21099b22
ramosjm <jjramos_2004@hotmail.com>
2019-02-08 18:36:25 +0000

    get merchant id test padd.

--- little_bits_app/models/specs/merchant_spec.rb
+++ little_bits_app/models/specs/merchant_spec.rb
@@ -11,8 +11,12 @@ class MerchantTest < MiniTest::Test
       })
   end
 
-  def test_can_create_merchant
+  def test_can_create_merchant()
     assert_equal(Merchant,@merchant_1.class)
+  end
+
+  def test_get_merchant_id()
+    assert_equal(1,@merchant_1.id)
   end
 
 end
