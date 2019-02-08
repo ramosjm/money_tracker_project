231779a7a6cba46c81bb95fdc47a4359723ad0bd
ramosjm <jjramos_2004@hotmail.com>
2019-02-08 16:27:48 +0000

    tracker.sql complete.

---
+++ little_bits_app/db/tracker.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS merchants;
+DROP TABLE IF EXISTS tags;
+
+
+CREATE TABLE tags(
+  id SERIAL4 PRIMARY KEY,
+  tag VARCHAR(255)
+);
+
+CREATE TABLE merchants(
+id SERIAL4 PRIMARY KEY,
+name VARCHAR(255)
+);
+
+CREATE TABLE transactions(
+  id SERIAL4 PRIMARY KEY,
+  amount NUMERIC,
+  tag_id INT4,
+  merchant_id INT4
+);
