42beec7d783b88e49fec8eb79ed9a3fb5f6592f6
ramosjm <jjramos_2004@hotmail.com>
2019-02-09 12:00:46 +0000

    added routes to merchant controller - WIP.

---
+++ little_bits_app/controllers/merchants_controller.rb
@@ -0,0 +1,35 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require_relative('controllers/merchants_controller')
+
+get '/merchants/' do
+  erb(:index)
+end
+
+get '/merchants/:id' do
+  erb(:show)
+end
+
+get '/merchants/new' do
+  erb(:new)
+end
+
+post '/merchants' do
+  #save merchant here
+  erb(:create)
+end
+
+get '/merchants/:id/edit' do
+  #use find method
+  erb(:edit)
+end
+
+post '/merchants/:id' do
+  #use update method
+  redirect to '/merchants/'
+end
+
+post '/merchants/:id/delete' do
+  # use find(id) and .Delete
+  redirect to '/merchants/'
+end
