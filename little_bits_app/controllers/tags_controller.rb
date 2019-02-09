13a3ec1a535febf825883fb91b7f9b812f474562
ramosjm <jjramos_2004@hotmail.com>
2019-02-09 18:00:17 +0000

    added routes to tags controller.

---
+++ little_bits_app/controllers/tags_controller.rb
@@ -0,0 +1,35 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require_relative('controllers/merchants_controller')
+
+get '/tags/' do
+  erb(:index)
+end
+
+get '/tags/:id' do
+  erb(:show)
+end
+
+get '/tags/new' do
+  erb(:new)
+end
+
+post '/tags' do
+  #save merchant here
+  erb(:create)
+end
+
+get '/tags/:id/edit' do
+  #use find method
+  erb(:edit)
+end
+
+post '/tags/:id' do
+  #use update method
+  redirect to '/tags/'
+end
+
+post '/tags/:id/delete' do
+  # use find(id) and .Delete
+  redirect to '/tags/'
+end
