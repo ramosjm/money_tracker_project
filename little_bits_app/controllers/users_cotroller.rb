7c122e538a90d5a7e45709b14266b2a3fcf74753
ramosjm <jjramos_2004@hotmail.com>
2019-02-12 10:38:37 +0000

    user controllers updated with routes.

--- little_bits_app/controllers/users_cotroller.rb
+++ little_bits_app/controllers/users_cotroller.rb
@@ -4,6 +4,37 @@ require_relative('../models/user.rb')
 also_reload('../models/*')
 
 
+get'/users/' do
+ @users = User.all
+ erb(:"user/index")
+end
+
+get'/users/new/' do
+  erb(:"user/new")
+end
+
+post '/users/new/' do
+  @user = User.new(params)
+  @user.save
+  erb(:"user/create")
+end
+
 get '/users/:id' do
   erb(:"user/show")
+end
+
+get '/users/:id/edit' do
+  @user = User.find(params[:id])
+  erb(:"user/edit")
+end
+
+post '/users/:id/edit' do
+  User.new(params).update
+  redirect to '/users/'
+end
+
+post '/users/:id/delete' do
+  user = user.find(params[:id])
+  user.delete
+  redirect to '/users/'
 end
