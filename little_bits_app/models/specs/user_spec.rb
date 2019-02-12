bf42f4b6809e47694f2175d7a324ebd543a9c329
ramosjm <jjramos_2004@hotmail.com>
2019-02-12 09:43:19 +0000

    user class test passed.

--- little_bits_app/models/specs/user_spec.rb
+++ little_bits_app/models/specs/user_spec.rb
@@ -7,9 +7,13 @@ class UserTest < MiniTest::Test
   def setup()
     @user_1 = User.new({
       'first_name' => 'will',
-      'last_name' => 'smith'
+      'last_name' => 'smith',
       'budget' => 1320.40
       })
+  end
+
+  def test_can_create_user()
+    assert_equal(User,@user_1.class)
   end
 
 end
